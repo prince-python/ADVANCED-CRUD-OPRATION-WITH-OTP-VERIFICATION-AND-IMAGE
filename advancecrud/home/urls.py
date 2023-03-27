470ddd88ae134633b25b2a1dbfea15015cf35f20
Prince JAT <choudharyprince140@gmail.com>
2023-03-27 16:32:01 +0530

    Add files via upload

--- advancecrud/home/urls.py
+++ advancecrud/home/urls.py
@@ -5,7 +5,7 @@ from .import views
 urlpatterns = [
     path('',views.index),
     path('image_data/',views.image_data),
-    path('login',views.login,name='login'),
+    path('login/',views.login,name='login'),
      path('show/',views.datashow,name='show'),
     path("verifyotp",views.verifyotp,name="verifyotp"),
     path('delete/<int:id>',views.delete,name='delete'),
